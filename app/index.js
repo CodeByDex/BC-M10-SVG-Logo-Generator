7f3e2f0bea6e86ced0bc924a6d6acf78c0faabc0
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-04-01 11:46:47 -0400

    Added validations to color selection

--- app/index.js
+++ app/index.js
@@ -1,6 +1,19 @@
 const p = require("inquirer");
 const svg = require("./lib/SVG");
 
+const ColorKeyWords = [
+    { name: "Black", value: "#000000"},
+    { name: "White", value: "#FFFFFF"},
+    { name: "Red", value: "#FF0000"},
+    { name: "Orange", value: "#ffa500"},
+    { name: "Yellow", value: "#FFFF00"},
+    { name: "Green", value: "#008000"},
+    { name: "Blue", value: "#0000FF"},
+    { name: "Indigo", value: "#4b0082"},
+    { name: "Violet", value: "#ee82ee"},
+    { name: "Custom", value: "Custom"}
+]
+
 p
     .prompt(
         [{
@@ -12,14 +25,15 @@ p
             name: "textColor",
             message: "What is the logo text's color?",
             type: "list",
-            choices: ["White", "Black", "Custom"]
+            choices: ColorKeyWords
         },{
             name: "textColorCustom",
             message: "Please Enter a Hex Color Value",
             type: "input",
             when: (ans) => {
                 return ans.textColor === "Custom";
-            }
+            },
+            validate: svg.IsValidHexColor
         },{
             name: "shape",
             message: "What is the logo shape",
@@ -29,14 +43,15 @@ p
             name: "shapeColor",
             message: "What is the logo background color?",
             type: "list",
-            choices: ["White", "Black", "Custom"]
+            choices: ColorKeyWords
         },{
             name: "shapeColorCustom",
             message: "Please Enter a Hex Color Value",
             type: "input",
             when: (ans) => {
                 return ans.shapeColor === "Custom";
-            }
+            },
+            validate: svg.IsValidHexColor
         },]
     )
     .then((ans) => {
